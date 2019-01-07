8bdd987945c5067b64cafa75a0415716fac6b1f0
IMH51 <ianmartinharrison@outlook.com>
2019-01-07 18:44:57 +0000

    Completed js map lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver){ return driver.toLowerCase()})
+}
+
+function nameToAttributes(drivers) {
+  const splitDrivers = []
+  drivers.map(function (driver) {
+    var split = driver.split(" ")
+    let driverObject = {}
+    driverObject["firstName"] = split[0]
+    driverObject["lastName"] = split[1]
+    splitDrivers.push(driverObject)
+  })
+  return splitDrivers
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
