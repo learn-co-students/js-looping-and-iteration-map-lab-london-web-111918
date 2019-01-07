43b618a35873094c04baeb5817b03618432cfeb4
NathanB09 <nathanbell09@gmail.com>
2019-01-07 11:41:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file.
+function lowerCaseDrivers(collection) {
+  const newArr = collection.map(function (name) {
+    return name.toLowerCase();
+  }).slice();
+  return newArr;
+}
+
+function nameToAttributes(collection) {
+  const newArr = [];
+  collection.map(function (name) {
+    let firstLast = name.split(" ");
+    newArr.push({firstName: firstLast[0], lastName: firstLast[1]});
+  });
+  return newArr;
+}
+
+function attributesToPhrase(collection) {
+  const newArr = [];
+  collection.map(function (person) {
+    newArr.push(`${person.name} is from ${person.hometown}`);
+  });
+  return newArr;
+}
