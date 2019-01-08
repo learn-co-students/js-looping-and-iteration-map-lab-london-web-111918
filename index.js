beb34f0058236f366d9ecebcffbbee68a09b18c3
Kevmsutton <kevmsutton@gmail.com>
2019-01-08 17:59:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file.
+drivers = [
+{ firstName: 'Bobby', lastName: 'Smith' },
+{ firstName: 'Sammy', lastName: 'Watkins' },
+{ firstName: 'Sally', lastName: 'Jenkins' },
+{ firstName: 'Annette', lastName: 'Sawyer' },
+{ firstName: 'Sarah', lastName: 'Hucklebee' },
+{ firstName: 'bobby', lastName: 'ridge' }
+]
+
+function lowerCaseDrivers(drivers){
+  return drivers.map(driver => {
+    return driver.toLowerCase()
+  });
+}
+
+
+function nameToAttributes(drivers) {
+  return drivers.map(driver => {
+    let name = driver.split(" ");
+    return Object.assign({}, {firstName: name[0]}, {lastName: name[1]});
+  })
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(driver => {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
