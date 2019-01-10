8b363488f713cb47af1c5ed789265d3d9b68e16e
Metahaem <tom.rowson@gmail.com>
2019-01-10 16:33:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file.
+//this function takes an array of drivers,
+//and returns an array of the drivers names in lowercase.
+function lowerCaseDrivers(drivers) {
+  return drivers.map( x => x.toLowerCase());
+}
+
+function nameToAttributes(list) {
+  return list.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map( driver => `${driver.name} is from ${driver.hometown}`);
+}
